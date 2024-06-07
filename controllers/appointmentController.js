e56ab32dafa15fd3654cbb397d10ce05412de57c
JARP06 <143737595+JARP06@users.noreply.github.com>
2024-06-06 21:05:56 -0500

    trial

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -57,7 +57,6 @@ export async function allAppointments(req, res) {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const offset = (page - 1) * limit;
-
     try {
         // Query to count total records
         const countQuery = `
